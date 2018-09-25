95793de322660257b0d4057ef92a384d217efcb4
HG <HG@192.168.152.134>
2018-09-25 10:34:26 +0800

    项目中使用单节点 eureka-server demo

---
+++ src/main/java/com/ql/hg/UserEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.ql.hg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class UserEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserEurekaServerApplication.class, args);
+	}
+}
